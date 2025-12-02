7b1b29cda2e16c7ac754091d944804ea66edf3fd
Luiz <luiz.victor9973@gmail.com>
2025-12-01 21:04:43 -0300

    adicioned comments

--- main.py
+++ main.py
@@ -7,18 +7,21 @@ from strands import Agent
 from strands.models.ollama import OllamaModel
 from strands_tools import calculator
 
+# Carrega as variáveis de ambiente
 load_dotenv()
 
+# Configura o modelo do ollama
 ollama_model = OllamaModel(
     host=os.getenv("OLLAMA_BASE_URL"),
     model_id=os.getenv("LLM_MODEL"),
 )
 
 
-class Message(BaseModel):
+class Message(BaseModel):  # Como o  FastAPI irá receber o json
     message: str
 
 
+# Inicialização do app/agent
 app = FastAPI()
 agent = Agent(model=ollama_model, tools=[calculator], system_prompt=(
     "Você é um assistente útil. "
@@ -27,7 +30,7 @@ agent = Agent(model=ollama_model, tools=[calculator], system_prompt=(
 ))
 
 
-@app.post("/chat")
+@app.post("/chat")  # Endpoint da api
 def get_request(payload: Message):
     result = agent(payload.message)
     final_text = result.message["content"][0]["text"]
